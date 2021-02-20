afdc809e8af421deaefd9901b65b5ba48d2b1de8
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-02-20 14:41:45 +1100

    feat: add DefaultDockerFunction

---
+++ src/constructs/DefaultDockerFunction.ts
@@ -0,0 +1,23 @@
+import {
+  DockerImageFunction,
+  DockerImageFunctionProps
+} from "@aws-cdk/aws-lambda";
+import { RetentionDays } from "@aws-cdk/aws-logs";
+import { Construct, Duration } from "@aws-cdk/core";
+
+export class DefaultDockerFunction extends DockerImageFunction {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: Pick<DockerImageFunctionProps, "code"> &
+      Partial<DockerImageFunctionProps>
+  ) {
+    super(scope, id, {
+      memorySize: 128,
+      timeout: Duration.seconds(30),
+      logRetention: RetentionDays.ONE_WEEK,
+      retryAttempts: 0,
+      ...props
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { StaticSite } from "./constructs/StaticSite";
 export { BucketConfig } from "./constructs/BucketConfig";
 export { DefaultFunction } from "./constructs/DefaultFunction";
+export { DefaultDockerFunction } from "./constructs/DefaultDockerFunction";

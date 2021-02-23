144e0f27a51cea68a21410a0434bbb4ae618b040
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-02-24 00:31:35 +1100

    feat: make FunctionQueue public

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { StaticSite } from "./constructs/StaticSite";
 export { BucketConfig } from "./constructs/BucketConfig";
 export { DefaultFunction } from "./constructs/DefaultFunction";
 export { DefaultDockerFunction } from "./constructs/DefaultDockerFunction";
+export { FunctionQueue } from "./constructs/FunctionQueue";
